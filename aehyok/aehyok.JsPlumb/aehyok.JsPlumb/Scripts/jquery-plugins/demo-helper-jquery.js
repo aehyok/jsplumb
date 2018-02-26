6eb1ba35fb04b36002821a156e4733ac117dcb59
刘启明 <aehyok@vip.qq.com>
2018-02-26 18:10:16 +0800

    添加资源图片文件

---
+++ aehyok/aehyok.JsPlumb/aehyok.JsPlumb/Scripts/jquery-plugins/demo-helper-jquery.js
@@ -0,0 +1,21 @@
+﻿jsPlumb.bind("ready", function () {
+	// chrome fix.
+	document.onselectstart = function () { return false; };
+	// render mode
+	var resetRenderMode = function (desiredMode) {
+		var newMode = jsPlumb.setRenderMode(desiredMode);
+		$(".rmode").removeClass("selected");
+		$(".rmode[mode='" + newMode + "']").addClass("selected");
+		$(".rmode[mode='canvas']").attr("disabled", !jsPlumb.isCanvasAvailable());
+		$(".rmode[mode='svg']").attr("disabled", !jsPlumb.isSVGAvailable());
+		$(".rmode[mode='vml']").attr("disabled", !jsPlumb.isVMLAvailable());
+		nodeFlow.init();
+	};
+	$(".rmode").bind("click", function () {
+		var desiredMode = $(this).attr("mode");
+		if (jsPlumbDemo.reset) jsPlumbDemo.reset();
+		jsPlumb.reset();
+		resetRenderMode(desiredMode);
+	});
+	resetRenderMode(jsPlumb.SVG);
+});
